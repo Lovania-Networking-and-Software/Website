358fb404259f269e3981558225a9924fb480e0de
Desto2000 <nNn%&5sAu4FiJkrv@5$u@KSHHxbL5ui@x@783Z5R$B!5^gTPtztQ*@6&jPE4%z8q>
2023-06-16 10:59:52 +0300

    First update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from "@astrojs/cloudflare"
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: cloudflare(),
 	site: 'https://lovania.dev',
 	integrations: [mdx(), sitemap()],
 	routes: {

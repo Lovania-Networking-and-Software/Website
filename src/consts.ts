93dde7ddb59058e2588319890f0ce9f65e56d8a0
Desto2000 <nNn%&5sAu4FiJkrv@5$u@KSHHxbL5ui@x@783Z5R$B!5^gTPtztQ*@6&jPE4%z8q>
2023-06-17 21:08:55 +0300

    First update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Lovania Dev Blog';
-export const SITE_DESCRIPTION = 'Welcome to Lovania Dev Blog';
+export const SITE_TITLE = 'Lovania';
+export const SITE_DESCRIPTION = 'Welcome to Lovania';
